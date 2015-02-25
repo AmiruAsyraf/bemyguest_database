7b1e250760a5a1d23e9ed6fdc47eaadfac20e19b
iqlaas <kamaliqlaas@gmail.com>
2015-02-25 14:45:10 +0800

    list for welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,18 @@
 class WelcomeController < ApplicationController
 	def index
-		
+
 	end
+
+  def package_list
+    @packages = Package.all
+  end
+
+  def service_list
+    @services = Service.all
+  end
+
+  def agent_list
+    @agents = Agent.all
+  end
 
 end
