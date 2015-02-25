6ad1a26c8a164e2d9ff78a3e862c32d6060803dd
Ahmad Syahir Bin Muhamad <ahmadsyahirmuhamad@gmail.com>
2015-02-25 18:09:31 +0800

    add slide banner design word

--- app/assets/javascripts/syahir.js
+++ app/assets/javascripts/syahir.js
@@ -1,9 +1,7 @@
 $( document ).ready(function() {
 
 hide_menu();
-show_menu();
-
-
+show_menu();	
 
 	function hide_menu(){
 	// window.location.reload(); 
@@ -17,10 +15,12 @@ show_menu();
 		var traveller = "/users/traveller";
 		var agent = "/users/agent";
 		var info = "/welcome/";
+		var main = "/";
 
 		var result_traveller = url.match(traveller);
 		var result_agent = url.match(agent);
 		var result_info = url.match(info);
+		var result_main = url.match(main);
 
 		if (result_traveller == traveller) {
 			$('#traveller_menu').show();
@@ -31,5 +31,22 @@ show_menu();
 		if (result_info == info) {
 			$('#info_menu').show();
 		};
+		if (result_main == main) {
+			change();
+		};
+	};
+
+	function change(){
+	var text = ["Awesome Travelling Experience", "Really man awesome", "Bundle You Jing Install"];
+	    var counter = 0;
+	    var elem = document.getElementById("changeText");
+	    setInterval(change, 4000);
+	    function change() {
+	    	$('#changeText').slideDown(2000);
+	     elem.innerHTML = text[counter];
+	     	$('#changeText').slideUp(2000);
+	        counter++;
+	        if(counter >= text.length) { counter = 0; }
+	    }
 	};
 });
