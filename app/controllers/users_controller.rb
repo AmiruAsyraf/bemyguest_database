0f65b2cd25c48fb4485800a1bd1e1d639faedf19
Ahmad Syahir Bin Muhamad <ahmadsyahirmuhamad@gmail.com>
2015-02-25 10:15:43 +0800

    front end agent first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,5 @@
 class UsersController < ApplicationController
 
-def traveller_wishlist
-end
 
-def traveller_profile
-end
-
-def traveller_profile_update
-end
-
-def traveller_wishlist_update
-end
 
 end
